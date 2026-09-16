549a6a6bb454ad5398a78dd15a07588f7d90c0b2
jannie601H <jannie601@hufs.ac.kr>
2026-09-16 17:33:44 +0900

    optimize_rotation and eval_ptq groupsize modified from 128 to 64 ; for llama 3.2 1B and output directory added

--- scripts/10_optimize_rotation.sh
+++ scripts/10_optimize_rotation.sh
@@ -7,11 +7,11 @@
 
 # nnodes determines the number of GPU nodes to utilize (usually 1 for an 8 GPU node)
 # nproc_per_node indicates the number of GPUs per node to employ.
-torchrun --nnodes=1 --nproc_per_node=8 optimize_rotation.py \
+torchrun --nnodes=1 --nproc_per_node=1 optimize_rotation.py \
 --input_model $1  \
---output_rotation_path "your_path" \
---output_dir "your_output_path/" \
---logging_dir "your_log_path/" \
+--output_rotation_path "results/output_rotation/" \
+--output_dir "results/output/" \
+--logging_dir "results/logs" \
 --model_max_length 2048 \
 --fp16 False \
 --bf16 True \
@@ -23,7 +23,7 @@ torchrun --nnodes=1 --nproc_per_node=8 optimize_rotation.py \
 --lr_scheduler_type "cosine" \
 --gradient_checkpointing True \
 --save_safetensors False \
---max_steps 100 \
+--max_steps 10 \
 --w_bits $2 \
 --a_bits $3 \
 --k_bits $4 \
@@ -32,5 +32,5 @@ torchrun --nnodes=1 --nproc_per_node=8 optimize_rotation.py \
 --a_asym \
 --k_asym \
 --v_asym \
---k_groupsize 128 \
---v_groupsize 128 \
+--k_groupsize 64 \
+--v_groupsize 64 \
